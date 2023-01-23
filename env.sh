2f3caec094ade7e829ac516c9f540e38fa45e565
Siegfried Dubois <sidubois@grevillier.irisa.fr>
2023-01-23 17:33:32 +0100

    env

---
+++ env.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+. /local/env/envconda.sh
+
+
+WD=$(pwd)
+export CONDA_ALWAYS_YES="true"
+
+# init env
+conda create -p $WD"/.env" python=3.10
+
+# activate env to install packages
+conda activate $WD"/.env"
+
+# installing required python packages
+python -m pip install -r requirements.txt
+
+unset CONDA_ALWAYS_YES
+conda deactivate
