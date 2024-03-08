825c61e99679092336b30c5637a9e38ee5f82458
Tharos <dubois.siegfried@gmail.com>
2024-03-08 17:23:41 +0100

    ✨ Adding `multigrapher` implmentation

--- pancat/grapher.py
+++ pancat/grapher.py
@@ -1,9 +1,12 @@
 "Creates a graph we can navigate in."
 from os import path, remove
-from networkx import MultiDiGraph
+from json import load
+from networkx import MultiDiGraph, compose
 from pyvis.network import Network
 from pgGraphs import Graph
 from tharospytools.path_tools import path_allocator
+from pgGraphs import GFANetwork, Graph as pgGraph
+from tharospytools.list_tools import grouper
 
 
 def display_graph(graph: MultiDiGraph, colors_paths: dict[str, str], annotations: dict, output_path: str) -> None:
@@ -86,3 +89,155 @@ def compute_stats(
     except:
         pass
     return stats
+
+
+def multigraph_viewer(
+    file_A: str,
+    file_B: str,
+    file_editions: str,
+    boundaries: list,
+    output: str,
+) -> None:
+    """Renders a digraph alignment and explicits their connexions inbetween them.
+
+    Args:
+        file_A (str): first gfa file
+        file_B (str): second gfa file
+        file_editions (str): json file containing editions, must be graph-level
+        boundaries (list): list of node class sizes
+        output (str): output for html file
+    """
+    bounds = grouper(
+        [0] + [bound+x for bound in boundaries for x in [0, 1]] + [float('inf')], n=2, m=1
+    )
+
+    # Creating pgGraphs object
+    gfa_graph_A: pgGraph = pgGraph(
+        gfa_file=file_A,
+        with_sequence=True
+    )
+    gfa_graph_B: pgGraph = pgGraph(
+        gfa_file=file_B,
+        with_sequence=True
+    )
+
+    # Computing NetworkX structure
+    pangenome_graph_A: MultiDiGraph = GFANetwork.compute_networkx(
+        graph=gfa_graph_A,
+        node_prefix='A',
+        node_size_classes=bounds
+    )
+    pangenome_graph_B: MultiDiGraph = GFANetwork.compute_networkx(
+        graph=gfa_graph_B,
+        node_prefix='B',
+        node_size_classes=bounds
+    )
+
+    full_graph: MultiDiGraph = compose(
+        pangenome_graph_A,
+        pangenome_graph_B
+    )
+
+    editions: dict = load(open(file_editions, 'r', encoding='utf-8'))
+
+    for path, edition in editions.items():
+        current_counter_A: int = 0
+        current_counter_B: int = 0
+        node_index_A: int = 0
+        node_index_B: int = 0
+        for style in ['merges', 'splits']:
+            for pos, [node] in edition[style]:
+                while current_counter_A < pos:
+                    current_counter_A += gfa_graph_A.segments[gfa_graph_A.paths[path]
+                                                              ['path'][node_index_A][0]]['length']
+                    node_index_A += 1
+                while current_counter_B < pos:
+                    current_counter_B += gfa_graph_B.segments[gfa_graph_B.paths[path]
+                                                              ['path'][node_index_B][0]]['length']
+                    node_index_B += 1
+                if not full_graph.has_edge(
+                    f"A_{gfa_graph_A.paths[path]['path'][node_index_A-1][0]}",
+                    f"B_{gfa_graph_B.paths[path]['path'][node_index_B-1][0]}"
+                ):
+                    full_graph.add_edge(
+                        f"A_{gfa_graph_A.paths[path]['path'][node_index_A-1][0]}",
+                        f"B_{gfa_graph_B.paths[path]['path'][node_index_B-1][0]}",
+                        arrows='',
+                        alpha=.5,
+                        color='red' if style == 'merges' else 'blue'
+                    )
+    stats_A = compute_stats(
+        graph=gfa_graph_A,
+        length_classes=tuple(bounds)
+    )
+    stats_B = compute_stats(
+        graph=gfa_graph_B,
+        length_classes=tuple(bounds)
+    )
+
+    # Computing stats and displaying
+    display_graph(
+        graph=full_graph,
+        colors_paths={
+            **gfa_graph_A.metadata['colors'], **gfa_graph_B.metadata['colors']},
+        annotations={
+            **{f'A_{key}': val for key, val in stats_A.items()},
+            **{f'B_{key}': val for key, val in stats_B.items()}
+        },
+        output_path=output
+    )
+
+
+def graph_viewer(
+    file: str,
+    output: str,
+    boundaries: list
+) -> None:
+    bounds = grouper(
+        [0] + [bound+x for bound in boundaries for x in [0, 1]] + [float('inf')], n=2, m=1
+    )
+
+    # Creating pgGraphs object
+    gfa_graph: pgGraph = pgGraph(
+        gfa_file=file,
+        with_sequence=True
+    )
+
+    # Computing NetworkX structure
+    pangenome_graph: MultiDiGraph = GFANetwork.compute_networkx(
+        graph=gfa_graph,
+        node_prefix=None,
+        node_size_classes=bounds
+    )
+
+    # Computing stats and displaying
+    stats = compute_stats(
+        graph=gfa_graph,
+        length_classes=tuple(bounds)
+    )
+    display_graph(
+        graph=pangenome_graph,
+        colors_paths=gfa_graph.metadata['colors'],
+        annotations=stats,
+        output_path=output
+    )
+
+
+def graph_stats(file: str, boundaries: list) -> None:
+    bounds = grouper(
+        [0] + [bound+x for bound in boundaries for x in [0, 1]] + [float('inf')], n=2, m=1
+    )
+
+    # Creating pgGraphs object
+    gfa_graph: pgGraph = pgGraph(
+        gfa_file=file,
+        with_sequence=True
+    )
+
+    # Computing stats and displaying
+    graph_stats = compute_stats(
+        graph=gfa_graph,
+        length_classes=tuple(bounds)
+    )
+    for key, value in graph_stats.items():
+        print(f"{key}: {value}")
