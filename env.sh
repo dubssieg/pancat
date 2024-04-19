ced031a0239e8e735c3b3fdac717f15319f4f100
Tharos <dubois.siegfried@gmail.com>
2024-04-19 09:58:50 +0200

    📌 Automation on env creation

--- env.sh
+++ env.sh
@@ -1,18 +1,34 @@
 #!/bin/sh
 . /local/env/envconda.sh
 
+desired_env="pancat"
 
-WD=$(pwd)
-export CONDA_ALWAYS_YES="true"
-
-# init env
-conda create -p $WD"/.env" python=3.10
-
-# activate env to install packages
-conda activate $WD"/.env"
-
-# installing required python packages
-python setup.py install
-
-unset CONDA_ALWAYS_YES
-conda deactivate
+if conda env list | grep -q "\b$desired_env\b"; then
+    if conda info --envs | grep -q "^\s*$desired_env\b"; then
+        echo "Error: The '$desired_env' environment should be active."
+        echo "Please activate the '$desired_env' environment using:"
+        echo "conda activate $desired_env"
+        read -p "Do you want to activate environment $desired_env? ([Y]/n): " choice
+        if [[ "$choice" == "y" || -z "$choice" ]]; then
+            conda activate $desired_env
+        else
+            exit 1
+        fi
+    else
+        echo "The '$desired_env' environment is currently active."
+    fi
+else
+    read -p "The '$desired_env' environment does not exist. Do you want to create it? ([Y]/n): " choice
+    if [[ ${choice:-y} == y ]]; then
+        export CONDA_ALWAYS_YES="true"
+        conda create -n $desired_env python=3.11
+        conda activate $desired_env
+        python setup.py install
+        unset CONDA_ALWAYS_YES
+        conda deactivate
+        echo "The '$desired_env' environment has been created. Please activate it using:"
+        echo "conda activate $desired_env"
+    else
+        exit 1
+    fi
+fi
