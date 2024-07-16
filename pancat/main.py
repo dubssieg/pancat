772256a7168a4dd6f9157be5551d4a6a47d7fdb8
Tharos <dubois.siegfried@gmail.com>
2024-07-16 17:42:20 +0200

    📝 Updating commands help strings

--- pancat/main.py
+++ pancat/main.py
@@ -7,6 +7,7 @@ from pathlib import Path
 from rich import print
 from pgGraphs import Graph as pgGraph
 from tharospytools.path_tools import path_allocator
+from pancat.constants import *
 # TODO Done
 from pancat.offset_in_gfa import add_offsets_to_gfa
 from pancat.grapher import graph_stats, graph_viewer, multigraph_viewer
@@ -34,176 +35,294 @@ parser._optionals.title = 'Global Arguments'
 ## Subparser for isolate_by_range ##
 
 parser_isolate: ArgumentParser = subparsers.add_parser(
-    'isolate', help="Isolates a subgraph within a graph.\n""Relies on position in base pairs, requires the PO tag (built by pangraphs offset). In order to output a correct graph, you should provide a graph that has paths or walks to describe nodes chaining, and your range should be valid (meaning, base positions must be in the graph).")
-parser_isolate.add_argument("file", type=str, help="Path to a gfa-like file")
-parser_isolate.add_argument(
-    "out", type=str, help="Output path (with extension)")
-parser_isolate.add_argument(
-    '-s',
-    '--start',
-    type=int,
-    help='To specifiy a starting point (in bp) to create a subgraph',
+    'isolate',
+    help=HELP_COMMAND_ISOLATE,
 )
 parser_isolate.add_argument(
-    '-e',
-    '--end',
-    type=int,
-    help='To specifiy a end point (in bp) to create a subgraph',
+    "file",
+    type=str,
+    help=HELP_INPUT_FILE_WITH_PATHS,
+)
+parser_isolate.add_argument(
+    "out",
+    type=str,
+    help=HELP_OUTPUT_FILE_GFA,
 )
 parser_isolate.add_argument(
     '-r',
     '--reference',
     type=str,
-    help='To specifiy the path to follow',
+    help=HELP_INPUT_REFERENCE,
+)
+parser_isolate.add_argument(
+    '-s',
+    '--start',
+    type=int,
+    help=HELP_INPUT_START_POSITION,
+)
+parser_isolate.add_argument(
+    '-e',
+    '--end',
+    type=int,
+    help=HELP_INPUT_END_POSITION,
 )
 
 ## Subparser for offset_in_gfa ##
 
 parser_offset: ArgumentParser = subparsers.add_parser(
-    'offset', help="Add path offsets to a graph\n"
-    "Adds a JSON string, PO (Path Offset) positions, relative to paths. Hence, PO:J:{'w1':[(334,335,'+')],'w2':[(245,247,'-'),(336,338,'-')]} tells that the walk/path w1 contains the sequence starting at position 334 and ending at position 335, and the walk/path w2 contains the sequence starting at the offset 245 (ending 247) and crosses it a second time between position 336 and 338, and that the sequences are reversed one to each other. Note that any non-referenced walk in this field means that the node is not inside the given walk.")
-
-parser_offset.add_argument("file", type=str, help="Path to a gfa-like file")
+    'offset',
+    help=HELP_COMMAND_OFFSET,
+)
 parser_offset.add_argument(
-    "out", type=str, help="Output path (with extension)")
-
-## Subparser for linearize ##
-
-# parser_linear: ArgumentParser = subparsers.add_parser('linearize', help="Tries to simplify the graph structure, compressing superbubble chains in simple nodes.")
-# parser_linear.add_argument("file", type=str, help="Path to a gfa-like file")
-# parser_linear.add_argument("output", type=str,help="Output path for the gfa graph simplified file.")
+    "file",
+    type=str,
+    help=HELP_INPUT_FILE_WITH_PATHS,
+)
+parser_offset.add_argument(
+    "out",
+    type=str,
+    help=HELP_OUTPUT_FILE_GFA,
+)
 
 ## Subparser for grapher ##
 
 parser_grapher: ArgumentParser = subparsers.add_parser(
-    'grapher', help="Creates a html view of the graph.\n"
-    "Huge graphs may take long time to display, or might be messy. Advice would be to extract parts you want to display (with pangraphs isolate or pangraphs neighborhood for instance), then computes the vizualisation on the selected part.")
-
-parser_grapher.add_argument("file", type=str, help="Path to a gfa-like file")
-parser_grapher.add_argument("output", type=str,
-                            help="Output path for the html graph file.")
+    'grapher',
+    help=HELP_COMMAND_GRAPHER,
+)
 parser_grapher.add_argument(
-    "-b", "--boundaries", type=int, help="One or a list of ints to use as boundaries for display (ex : -b 50 2000 will set 3 colors : one for nodes in range 0-50bp, one for nodes in range 51-2000 bp and one for nodes in range 2001-inf bp).", nargs='+', default=[50])
-
-
-## Subparser for correct ##
-
-parser_correct: ArgumentParser = subparsers.add_parser(
-    'correct', help="(WIP, experimental) Corrects the graph by adding missing edges.")
-parser_correct.add_argument("file", type=str, help="Path to a gfa-like file")
-parser_correct.add_argument("output", type=str,
-                            help="Output path for the gfa graph file.")
-
+    "file",
+    type=str,
+    help=HELP_INPUT_FILE_GENERAL,
+)
+parser_grapher.add_argument(
+    "output",
+    type=str,
+    help=HELP_OUTPUT_FILE_HTML,
+)
+parser_grapher.add_argument(
+    "-b",
+    "--boundaries",
+    type=int,
+    help=HELP_PARAM_BOUNDARIES,
+    nargs='+',
+    default=[50],
+)
 
 ## Subparser for multigrapher ##
 
 parser_multigrapher: ArgumentParser = subparsers.add_parser(
-    'multigrapher', help="Creates a html view of the alignment of two graphs.\n"
-    "Huge graphs may take long time to display, or might be messy. Advice would be to extract parts you want to display (with pangraphs isolate or pangraphs neighborhood for instance), then computes the vizualisation on the selected part.")
-
+    'multigrapher',
+    help=HELP_COMMAND_MULTIGRAPHER,
+)
 parser_multigrapher.add_argument(
-    "file_A", type=str, help="Path to a first gfa-like file")
+    "file_A",
+    type=str,
+    help=HELP_INPUT_FILE_WITH_PATHS,
+)
 parser_multigrapher.add_argument(
-    "file_B", type=str, help="Path to a second gfa-like file")
+    "file_B",
+    type=str,
+    help=HELP_INPUT_FILE_WITH_PATHS,
+)
 parser_multigrapher.add_argument(
-    "editions", type=str, help="Path to a path-level edition file created with pancat edit")
+    "editions",
+    type=str,
+    help=HELP_INPUT_EDITION_FILE,
+)
 parser_multigrapher.add_argument(
-    "output", type=str,
-    help="Output path for the html graph file.")
+    "output",
+    type=str,
+    help=HELP_OUTPUT_FILE_HTML,
+)
 parser_multigrapher.add_argument(
-    "-b", "--boundaries", type=int, help="One or a list of ints to use as boundaries for display (ex : -b 50 2000 will set 3 colors : one for nodes in range 0-50bp, one for nodes in range 51-2000 bp and one for nodes in range 2001-inf bp).", nargs='+', default=[50])
+    "-b",
+    "--boundaries",
+    type=int,
+    help=HELP_PARAM_BOUNDARIES,
+    nargs='+',
+    default=[50],
+)
 
 ## Subparser for stats ##
 
 parser_stats: ArgumentParser = subparsers.add_parser(
-    'stats', help="Retrieves basic stats on a pangenome graph.")
-
-parser_stats.add_argument("file", type=str, help="Path to a gfa-like file")
+    'stats',
+    help=HELP_COMMAND_STATS,
+)
 parser_stats.add_argument(
-    "-b", "--boundaries", type=int, help="One or a list of ints to use as boundaries for display (ex : -b 50 2000 will set 3 colors : one for nodes in range 0-50bp, one for nodes in range 51-2000 bp and one for nodes in range 2001-inf bp).", nargs='+', default=[50])
+    "file",
+    type=str,
+    help=HELP_INPUT_FILE_GENERAL,
+)
+parser_stats.add_argument(
+    "-b",
+    "--boundaries",
+    type=int,
+    help=HELP_PARAM_BOUNDARIES,
+    nargs='+',
+    default=[50],
+)
 
 ## Subparser for assert_complete_pangenome ##
 
 parser_complete: ArgumentParser = subparsers.add_parser(
-    'complete', help="Asserts if the graph is a completet pangenome graph"
+    'complete',
+    help=HELP_COMMAND_COMPLETE,
 )
-
 parser_complete.add_argument(
-    "file", type=str, help="Path to a gfa-like file")
+    "file",
+    type=str,
+    help=HELP_INPUT_FILE_WITH_PATHS,
+)
 parser_complete.add_argument(
-    "pipeline", type=str, help="Tab-separated mapping between path names and path to files")
+    "pipeline",
+    type=str,
+    help=HELP_INPUT_PIPELINE,
+)
 
 
 ## Subparser for reconstruct_sequences ##
 
 parser_reconstruct: ArgumentParser = subparsers.add_parser(
-    'reconstruct', help="Reconstruct linear sequences from a GFA graph.\n"
-    "Given a GFA file with paths, reconstruct a linear sequence for each haplotype between two offsets."
+    'reconstruct',
+    help=HELP_COMMAND_RECONSTRUCT,
 )
-
 parser_reconstruct.add_argument(
-    "file", type=str, help="Path to a gfa-like file")
+    "file",
+    type=str,
+    help=HELP_INPUT_FILE_WITH_PATHS,
+)
 parser_reconstruct.add_argument(
-    "out", type=str, help="Path to a folder that will contain the extracted paths.")
+    "out",
+    type=str,
+    help=HELP_OUTPUT_FOLDER,
+)
 parser_reconstruct.add_argument(
-    "-s", "--split", help="Tells to split in different files", action='store_true')
+    "-s",
+    "--split",
+    help=HELP_PARAM_SPLIT,
+    action='store_true',
+)
 parser_reconstruct.add_argument(
-    "--selection", type=str, help="Name(s) for the paths you want to reconstruct.", nargs='*', default=True)
-
-
-## Subparser for cyclotron ##
-
-# parser_cycles: ArgumentParser = subparsers.add_parser('cycles', help="Extracts all cycles from the graph file, path by path.\n""It can return those as lists of nodes names, sequences, or lengths of chains.")
-
-# parser_cycles.add_argument("file", type=str, help="Path to a gfa-like file")
-# parser_cycles.add_argument("output", type=str, help="Path to folder or json file")
-# parser_cycles.add_argument("-m","--mode",help="Tells the return mode",required=True,choices=['names', 'sequences', 'lengths'])
+    "--selection",
+    type=str,
+    help=HELP_PARAM_PATHSELECT,
+    nargs='*',
+    default=True,
+)
 
 ## Subparser for edit_distance ##
 
 parser_edit: ArgumentParser = subparsers.add_parser(
-    'edit', help="Aligns graphs and tires to compute the minimal set of events that explains how we go from one graph to another.")
-
+    'edit',
+    help=HELP_COMMAND_EDIT,
+)
 parser_edit.add_argument(
-    "graph_A", type=str, help="Path to a GFA-like file.")
+    "graph_A",
+    type=str,
+    help=HELP_INPUT_FILE_WITH_PATHS,
+)
 parser_edit.add_argument(
-    "graph_B", type=str, help="Path to a GFA-like file.")
+    "graph_B",
+    type=str,
+    help=HELP_INPUT_FILE_WITH_PATHS,
+)
 parser_edit.add_argument(
-    "-o", "--output_path", required=True, type=str, help="Path to a .json output for results.")
+    "-o",
+    "--output_path",
+    required=True,
+    type=str,
+    help=HELP_OUTPUT_JSON_LOG,
+)
 parser_edit.add_argument(
-    "-p", "--pattern", type=str, help="Regexp to filer on for path names. Dealults to no filtering.")
+    "-p",
+    "--pattern",
+    type=str,
+    help=HELP_PARAM_REGEXP,
+)
 parser_edit.add_argument(
-    "-g", "--graph_level", help="Asks to perform edition computation at graph level.", action='store_true', default=False)
+    "-g",
+    "--graph_level",
+    help=HELP_PARAM_GRAPH_LEVEL,
+    action='store_true',
+    default=False,
+)
 parser_edit.add_argument(
-    "-c", "--cores", help="Number of cores for computing edition", type=int, default=1)
+    "-c",
+    "--cores",
+    help=HELP_PARAM_THREADS,
+    type=int,
+    default=1,
+)
 parser_edit.add_argument(
-    "-s", "--selection", type=str, help="Name(s) for the paths you want to compute edition on.", nargs='*', default=True)
+    "-s",
+    "--selection",
+    type=str,
+    help=HELP_PARAM_PATHSELECT,
+    nargs='*',
+    default=True,
+)
 parser_edit.add_argument(
-    "-t", "--trace_memory", help="Print to log file memory usage of data structures.", action='store_true', default=False)
+    "-t",
+    "--trace_memory",
+    help=HELP_PARAM_VERBOSE,
+    action='store_true',
+    default=False,
+)
 
 
 ## Subparser for compress_graph ##
 
 parser_compress: ArgumentParser = subparsers.add_parser(
-    'compress', help="Does a compression of the graph, merging simple non-conflicting bubbles .")
+    'compress',
+    help=HELP_COMMAND_COMPRESS,
+)
 parser_compress.add_argument(
-    "file", type=str, help="Path to a GFA-like file.")
+    "file",
+    type=str,
+    help=HELP_INPUT_FILE_WITH_PATHS,
+)
 parser_compress.add_argument(
-    "-o", "--output_file", required=True, type=str, help="Path to a .gfa output for results.")
+    "-o",
+    "--output_file",
+    required=True,
+    type=str,
+    help=HELP_OUTPUT_FILE_GFA,
+)
 parser_compress.add_argument(
-    "-m", "--minimize", help="Saves the graph with a minimal set of informations (for compatibiliy purposes)", action='store_true', default=False)
+    "-m",
+    "--minimize",
+    help=HELP_PARAM_MINIMIZE,
+    action='store_true',
+    default=False,
+)
 parser_compress.add_argument(
-    "-l", "--length", help="Maximum length of substitutions to compress", type=int, default=float('inf'))
+    "-l",
+    "--length",
+    help=HELP_PARAM_LENGTH,
+    type=int,
+    default=float('inf'),
+)
 
 ## Subparser for unfold ##
 
 parser_edit: ArgumentParser = subparsers.add_parser(
-    'unfold', help="Break cycles of a graph with P-lines or W-lines.")
-
+    'unfold',
+    help=HELP_COMMAND_UNFOLD,
+)
 parser_edit.add_argument(
-    "graph", type=str, help="Path to a >=GFA1-like file.")
+    "graph",
+    type=str,
+    help=HELP_INPUT_FILE_WITH_PATHS,
+)
 parser_edit.add_argument(
-    "-o", "--output_path", required=True, type=str, help="Path to a .gfa output for new graph.")
+    "-o",
+    "--output_path",
+    required=True,
+    type=str,
+    help=HELP_OUTPUT_FILE_GFA,
+)
 
 
 #######################################
@@ -282,12 +401,6 @@ def main() -> None:
     #                        REQUIRES IN-DEPTH TESING                            #
     ##############################################################################
 
-    elif args.subcommands == 'correct':
-        correct_graph(
-            args.file,
-            args.output,
-        )
-
     elif args.subcommands == 'compress':
         compress_graph(
             args.file,
@@ -332,7 +445,7 @@ def main() -> None:
             gfa_path=args.file,
             x=args.start,
             y=args.end,
-            sequence=args.reference,
+            sequence=args.reference.upper(),
             output=args.out
         )
 
